0c9f1c24fd677bf6825750a2155c8936e8a5fbc6
Puming Li <pumingli@gmail.com>
2018-08-29 09:32:29 -0500

    Testing calender lib

--- home/views.py
+++ home/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import calender
 
 def home(request):
+
+    cal = calendar.Calendar()
+    today = datetime.now().timetuple()
+
+    month = cal.itermonthdates(today.tm_year, today.tm_mon)
+
+
     return render(request, 'home.html')
