7b2bb6ae9e6c015fe6a02f7839ccd52c8ced9a90
Puming Li <pumingli@gmail.com>
2018-09-10 15:47:51 -0500

    fixed homeview

--- home/views.py
+++ home/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from graphos.sources.simple import SimpleDataSource
-from graphos.renderers.gchart import LineChart
-
 def home(request):
 
     return render(request, 'home.html')
