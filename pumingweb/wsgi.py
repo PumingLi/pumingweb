83914b469a1cd484e9357dcba835f2f180d8cadd
Puming Li <pumingli@gmail.com>
2019-09-11 02:46:52 -0500

    changed back wsgi.py

--- pumingweb/wsgi.py
+++ pumingweb/wsgi.py
@@ -1,22 +1,16 @@
-# """
-# WSGI config for pumingweb project.
-#
-# It exposes the WSGI callable as a module-level variable named ``application``.
-#
-# For more information on this file, see
-# https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
-# """
-#
-# import os
-#
-# from django.core.wsgi import get_wsgi_application
-#
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pumingweb.settings')
-#
-# application = get_wsgi_application()
+"""
+WSGI config for pumingweb project.
 
-def application(environ, start_response):
-    if environ['mod_wsgi.process_group'] != '':
-        import signal
-        os.kill(os.getpid(), signal.SIGINT)
-    return ["killed"]
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pumingweb.settings')
+
+application = get_wsgi_application()
